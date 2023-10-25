6e1a07f64f132ad812e65bf73c13a78f33f41833
jwang87 <jwang77@studentmail.nait.ca>
2023-10-24 22:27:14 -0600

    Formatting
    Formatted

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -29,7 +29,7 @@ const slugify = require('slugify');
 //                 console.log('File written 😍');
 //             });
 //         });
-//     })    
+//     })
 // });
 
 // console.log('Will read file');
@@ -37,54 +37,64 @@ const slugify = require('slugify');
 ///////////////////////////////////
 // Server
 
+const tempOverview = fs.readFileSync(
+  `${__dirname}/final/templates/template-overview.html`,
+  'utf-8'
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/final/templates/template-card.html`,
+  'utf-8'
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/final/templates/template-product.html`,
+  'utf-8'
+);
 
-const tempOverview = fs.readFileSync(`${__dirname}/final/templates/template-overview.html`, 'utf-8');
-const tempCard = fs.readFileSync(`${__dirname}/final/templates/template-card.html`, 'utf-8');
-const tempProduct = fs.readFileSync(`${__dirname}/final/templates/template-product.html`, 'utf-8');
-
-const data = fs.readFileSync(`${__dirname}/starter/dev-data/data.json`, 'utf-8');
+const data = fs.readFileSync(
+  `${__dirname}/starter/dev-data/data.json`,
+  'utf-8'
+);
 const dataObj = JSON.parse(data);
 
-const slug = dataObj.map(el => slugify(el.productName, { lower: true}));
+const slug = dataObj.map((el) => slugify(el.productName, { lower: true }));
 
 console.log(slug);
 
 const server = http.createServer((req, res) => {
-    const {query, pathname} = url.parse(req.url, true);
-    
-    // Overview page
-    if(pathname == '/overview' || pathname == '/'){
-        res.writeHead(200, {'Content-type': 'text/html'});
+  const { query, pathname } = url.parse(req.url, true);
 
-        const cardsHtml = dataObj.map(obj => replaceTemp(tempCard, obj)).join('');
-        //console.log(cardsHtml);
+  // Overview page
+  if (pathname == '/overview' || pathname == '/') {
+    res.writeHead(200, { 'Content-type': 'text/html' });
 
-        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml)
-        res.end(output);
-    
+    const cardsHtml = dataObj.map((obj) => replaceTemp(tempCard, obj)).join('');
+    //console.log(cardsHtml);
+
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+    res.end(output);
+
     // Product page
-    } else if(pathname == '/product') {
-        //this is a test
-        const product = dataObj[query.id];
-        const output = replaceTemp(tempProduct, product);
-        res.end(output);
+  } else if (pathname == '/product') {
+    //this is a test
+    const product = dataObj[query.id];
+    const output = replaceTemp(tempProduct, product);
+    res.end(output);
 
     //API
-    } else if (pathname == '/api') {
-        res.writeHead(200, {'Content-type': 'application/json'});
-        res.end(data);
-    
+  } else if (pathname == '/api') {
+    res.writeHead(200, { 'Content-type': 'application/json' });
+    res.end(data);
+
     //NOT FOUND
-    } else {
-        res.writeHead(404, {
-            'Content-type': 'text/html',
-            'my-own-header': 'Hello-World'
-        });
-        res.end('<h1>This page cannot be found!</h1>')
-    }
-        
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-own-header': 'Hello-World',
+    });
+    res.end('<h1>This page cannot be found!</h1>');
+  }
 });
 
 server.listen(8081, '127.0.0.1', () => {
-    console.log('Listening on port 8081')
+  console.log('Listening on port 8081');
 });
