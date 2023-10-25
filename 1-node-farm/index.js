398214c2d32f9459f5277b1549ebfc18bf864297
jwang87 <jwang77@studentmail.nait.ca>
2023-10-24 21:51:18 -0600

    complete
    complete

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -2,6 +2,9 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
+const replaceTemp = require('./moules/replaceTemplate');
+const slugify = require('slugify');
+
 ///////////////////////////
 // File
 
@@ -33,23 +36,7 @@ const url = require('url');
 
 ///////////////////////////////////
 // Server
-const replaceTemp = (temp, product) => {
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
 
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%FORM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%ID%}/g, product.id);
-
-    if(!product.organic) {
-        output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-    }
-
-    return output;
-}
 
 const tempOverview = fs.readFileSync(`${__dirname}/final/templates/template-overview.html`, 'utf-8');
 const tempCard = fs.readFileSync(`${__dirname}/final/templates/template-card.html`, 'utf-8');
@@ -58,24 +45,32 @@ const tempProduct = fs.readFileSync(`${__dirname}/final/templates/template-produ
 const data = fs.readFileSync(`${__dirname}/starter/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
+const slug = dataObj.map(el => slugify(el.productName, { lower: true}));
+
+console.log(slug);
+
 const server = http.createServer((req, res) => {
-    const pathName = req.url;
+    const {query, pathname} = url.parse(req.url, true);
+    
     // Overview page
-    if(pathName == '/overview' || pathName == '/'){
+    if(pathname == '/overview' || pathname == '/'){
         res.writeHead(200, {'Content-type': 'text/html'});
 
         const cardsHtml = dataObj.map(obj => replaceTemp(tempCard, obj)).join('');
-        console.log(cardsHtml);
+        //console.log(cardsHtml);
 
         const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml)
         res.end(output);
     
     // Product page
-    } else if(pathName == '/product') {
-        res.end('This is the product');
+    } else if(pathname == '/product') {
+        //this is a test
+        const product = dataObj[query.id];
+        const output = replaceTemp(tempProduct, product);
+        res.end(output);
 
     //API
-    } else if (pathName == '/api') {
+    } else if (pathname == '/api') {
         res.writeHead(200, {'Content-type': 'application/json'});
         res.end(data);
     
